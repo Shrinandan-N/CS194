33b00b1fa2d98be4a6a6f4dfd7d52e3e0074d3b7
Shrinandan Narayanan <emailshri.n@gmail.com>
2024-12-16 18:52:25 -0800

    add search

--- reddit.py
+++ reddit.py
@@ -1,15 +1,25 @@
+from webbrowser import get
 import praw
-import FastAPI
 
+def get_100_subreddits(keywords):
+    reddit = praw.Reddit(client_id="0MFmtUnQVteI8igkmZIO4A",
+                         client_secret="dL5Ud4N_aH2lzTggcGhWDCZVn6_aAg",
+                         user_agent="ToastyPostyBot/1.0 by u/One-Cap-3906")
+    subreddits = []
+    for keyword in keywords:
+        subreddits.extend(reddit.subreddit(keyword).hot(limit=100))
 
-def main():
+    subreddits = [subreddit.subreddit.display_name for subreddit in subreddits]
+
+    return subreddits
+
+def post_to_reddit(subreddit, title, content):
     client_id = "0MFmtUnQVteI8igkmZIO4A"
     client_secret = "dL5Ud4N_aH2lzTggcGhWDCZVn6_aAg"
-    username = "One-Cap-3906"  # Your Reddit username
-    password = "toastyposty123"  # Replace with your Reddit password
+    username = "One-Cap-3906"  
+    password = "toastyposty123"  
     user_agent = "ToastyPostyBot/1.0 by u/One-Cap-3906"
 
-    # Initialize Reddit instance
     reddit = praw.Reddit(
         client_id=client_id,
         client_secret=client_secret,
@@ -18,12 +28,11 @@ def main():
         user_agent=user_agent
     )
 
-    # Choose subreddit and submit post
-    subreddit = reddit.subreddit("test")
-    title = "Hello Reddit this is Jatin Devireddy from Goyal Industries"
-    content = "Justin Narayanan says hello!"
+    subreddit = reddit.subreddit(subreddit)
+    title = title
+    content = content
 
     submission = subreddit.submit(title=title, selftext=content)
     print(f"Post submitted! URL: {submission.url}")
 
-main()
+print("SUBREDDITS: ", get_100_subreddits(["food"]))
